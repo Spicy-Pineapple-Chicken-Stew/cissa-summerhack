19f208d91e6a6450f6c53e7ea4fed8d8ed517401
blackberryfound <86906130+blackberryfound@users.noreply.github.com>
2022-01-16 11:53:31 +1100

    Home page
    Beginning layout for homepage

---
+++ src/Pages/Home.js
@@ -0,0 +1,17 @@
+function Home(props){
+
+    return(
+        <div>
+            <div className="main_heading">
+                Website name
+                <div className="main_text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tellus leo, feugiat in tortor sollicitudin, rhoncus lobortis orci. Integer eu enim lectus. Nunc porta lectus sed velit ullamcorper auctor. Donec fermentum facilisis mattis.
+                </div>
+            </div>
+            
+        </div>
+    )
+
+}
+
+export default Home;
