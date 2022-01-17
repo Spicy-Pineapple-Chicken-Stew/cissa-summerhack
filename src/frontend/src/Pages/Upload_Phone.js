591cae98c7297d376728661f6c6e56888ffbffa0
blackberryfound <86906130+blackberryfound@users.noreply.github.com>
2022-01-17 22:14:47 +1100

    phone upload start

---
+++ src/frontend/src/Pages/Upload_Phone.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import ButtonBases from '../Components/Imagebutton.js'
+
+function Home(props){
+
+    return(
+        <div className = 'wrapper'>
+            <ButtonBases></ButtonBases>
+        </div>
+    )
+
+}
+
+export default Home;
