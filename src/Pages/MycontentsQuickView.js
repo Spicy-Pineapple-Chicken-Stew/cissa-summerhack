57de0184a1eb07c87ffd1cfa690643abf48bfcdc
blackberryfound <86906130+blackberryfound@users.noreply.github.com>
2022-01-21 23:59:16 +1100

    Contents quick view page (NEED TO ADD SECTION TITLES)

--- src/Pages/MycontentsQuickView.js
+++ src/Pages/MycontentsQuickView.js
@@ -8,41 +8,105 @@ import FormControl from '@mui/material/FormControl';
 import FormHelperText from '@mui/material/FormHelperText';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import {CustomTextField} from '../Components/CustomTextField';
-
+import { styled } from '@mui/material/styles';
 
 export default function MC_detailedview(){
     return (
         <div>
-            <div></div>
-            <Typography align='left' variant='h2' ml={20} fontFamily={'Oxygen'} color={'rgba(22, 105, 122, 1)'}>
-                <Box>Title</Box>
-            </Typography>
-            <Typography align='left' variant='subtitle1' ml={20}>
-                <Box>
-                    Change view: Default
-                    <Switch />
-                    Detailed
+            <div>      
+                <Box sx =
+                {{flexWrap: 'wrap', 
+                display:'flex', 
+                flexDirection:'column', 
+                alignContent: 'flex-start',
+                marginLeft: '3%',
+                marginTop: '5%',
+                border: '1px solid #000000'}}>
+                    <Typography 
+                    sx = {{
+                    fontSize:'300%',
+                    fontFamily: 'Oxygen',
+                    color: 'rgba(22, 105, 122, 1)'
+                }} >
+                    <Box 
+                    sx ={{flexWrap: 'wrap', 
+                        display:'flex', 
+                        flexDirection:'row'}}>
+                    
+                    Title</Box>
+
+                </Typography>
+                
+                <Typography variant='subtitle1'>
+                    <Box>
+                        Change view: Default
+                        <Switch />
+                        Detailed
+                    </Box>
+                </Typography>
                 </Box>
-            </Typography>
-            <div>
-            <Box sx={{ display: 'block', alignItems: 'center', display:'flex'}} >
-            <Typography variant='subtitle1' ml={20}>
-                <Box
-                    sx={{
-                        width: 150,
-                        height: 300,
-                        border: '1px solid #489FB5',
-                        borderRadius: '4px',
-                        '&:hover': {
-                            border: '1px solid #16697A',
-                            opacity: 0.9,
-                        },
-                    }}>
-                    Text here
-                </Box>
-            </Typography>
+            </div>
+            <div style ={{margin:'3%', marginTop: '3%'}}>
+            
+           <Box sx ={{
+               position: 'relative',
+               flexWrap: 'wrap', 
+               display:'flex', 
+               flexDirection:'row', 
+               alignContent: 'flex-start',
+               justifyContent: 'flex-start',
+            }}>
+            
+            <Box
+                sx={{
+                    width: '60%',
+                    height: 130,
+                    border: '1px solid #489FB5',
+                    marginRight: '2%',
+                    borderRadius: '4px',
+                    flexShrink: 1,
+                    '&:hover': {
+                        border: '1px solid #16697A',
+                        opacity: 0.9,
+                    },
+                    
+                }}>
+                <Typography variant='subtitle1'>Text here</Typography>
             </Box>
-        </div>
+            
+            <Box
+                sx={{
+                    position: 'relative',
+                    width: '37.5%',
+                    height: 130,
+                    border: '1px solid #489FB5',
+                    borderRadius: '4px',
+                    '&:hover': {
+                        border: '1px solid #16697A',
+                        opacity: 0.9,
+                    },
+                }}>
+                <Typography variant='subtitle1'>Text here </Typography>
+            </Box>
+
+            <Box
+                sx={{
+                    marginTop: '3%',
+                    position: 'relative',
+                    width: '100%',
+                    height: 300,
+                    border: '1px solid #489FB5',
+                    borderRadius: '4px',
+                    '&:hover': {
+                        border: '1px solid #16697A',
+                        opacity: 0.9,
+                    },
+                }}>
+                <Typography variant='subtitle1'>Text here </Typography>
+            </Box>
+
+            </Box>
+            </div>
         </div>
     )
 }
