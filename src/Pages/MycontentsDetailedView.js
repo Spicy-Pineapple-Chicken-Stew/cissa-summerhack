0955cf5ffaa2fadd76abed59a7dd19d4c6a7b291
Mango23333 <75919015+Mango23333@users.noreply.github.com>
2022-01-21 22:14:00 +1100

    Added contents for summary, flash cards and original submission

--- src/Pages/MycontentsDetailedView.js
+++ src/Pages/MycontentsDetailedView.js
@@ -6,19 +6,20 @@ import MC_Sidemenu from '../Components/MycontentsDetailedViewSidemenu';
 
 
 export default function MC_detailedview(){
+
     return (
         <div>
-            <Typography align='left' variant='h2' ml={20} fontFamily={'Oxygen'} color={'rgba(22, 105, 122, 1)'}>
-                <Box>Title</Box>
+            <Typography align='left' fontSize={'2vw'} ml={20} fontFamily={'Oxygen'} color={'rgba(22, 105, 122, 1)'}>
+                <Box>Studymaterialaccleratedmath2lec21.mp4</Box>
             </Typography>
-            <Typography align='left' variant='subtitle1' ml={20}>
+            <Typography align='left' fontSize={'1vw'} ml={20}>
                 <Box>
                     Change view: Default
                     <Switch />
                     Detailed
                 </Box>
             </Typography>
-            <Box sx={{width: 'auto', height: 'auto'}}>
+            <Box sx={{width: 'auto', height: 'auto'}}>                
                 <MC_Sidemenu />
             </Box>
         </div>
