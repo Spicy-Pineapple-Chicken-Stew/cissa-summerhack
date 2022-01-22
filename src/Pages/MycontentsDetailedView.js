490a3bae267542cc9eb34dc03ec6837dcf04b21b
Mango23333 <75919015+Mango23333@users.noreply.github.com>
2022-01-22 23:50:15 +1100

    Change how it looks

--- src/Pages/MycontentsDetailedView.js
+++ src/Pages/MycontentsDetailedView.js
@@ -9,10 +9,10 @@ export default function MC_detailedview(){
 
     return (
         <div>
-            <Typography align='left' fontSize={'2vw'} ml={20} fontFamily={'Oxygen'} color={'rgba(22, 105, 122, 1)'}>
+            <Typography align='left' fontSize={'2vw'} ml={'13.2vw'} fontFamily={'Oxygen'} color={'rgba(22, 105, 122, 1)'} mt={2}>
                 <Box>Studymaterialaccleratedmath2lec21.mp4</Box>
             </Typography>
-            <Typography align='left' fontSize={'1vw'} ml={20}>
+            <Typography align='left' fontSize={'1vw'} ml={'70vw'}>
                 <Box>
                     Change view: Default
                     <Switch />
