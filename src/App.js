d255a273fbbaa551d2d15228348169a2b3891a49
blackberryfound <86906130+blackberryfound@users.noreply.github.com>
2022-01-22 22:12:53 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,50 +11,41 @@ import LoginRegistration from "./Pages/LoginRegistration";
 import {UserContext} from "./Contexts/UserContext";
 import MycontentsQuickView from './Pages/MycontentsQuickView';
 
-function App() {
-    /*
-  let [currentPage, setCurrentPage] = useState('Home');
-  let [isMobile, setIsMobile] = useState(false);
-  let [user, setUser] = useState(null);
 
-    function handleWindowSizeChange() {
-        setIsMobile(window.innerWidth <= 768);
-    }
-    useEffect(() => {
-        window.addEventListener('resize', handleWindowSizeChange);
-        return () => {
-            window.removeEventListener('resize', handleWindowSizeChange);
-        }
-    }, []);
-    */
-  return (
-    <div>
-      <div>
-        <Global_MenuBar/>
-      </div>
-      <div className="App">
-        <MycontentsQuickView/>
-      </div>
-    </div>
-      
-      /*
-      <CurrentPageContext.Provider value={[currentPage, setCurrentPage]}>
-          <MobileContext.Provider value={[isMobile, setIsMobile]}>
-              <UserContext.Provider value={[user, setUser]}>
-                  <div>
-                      <div>
-                          {isMobile ? <Global_MenuBar_mobile/> : <Global_MenuBar />}
-                      </div>
-                      <div className="App">
-                          {currentPage === 'Home' && <Home/>}
-                          {currentPage === 'Upload' && !isMobile && <UploadDesktop/>}
-                          {currentPage === 'Upload' && isMobile && <UploadPhone />}
-                          {currentPage === 'Log in' && <LoginRegistration/>}
-                      </div>
-                  </div>
-              </UserContext.Provider>
-          </MobileContext.Provider>
-      </CurrentPageContext.Provider>*/
-    )
-}
-export default App;
+function App() {
+    let [currentPage, setCurrentPage] = useState('Home');
+    let [isMobile, setIsMobile] = useState(false);
+    let [user, setUser] = useState(null);
+  
+      function handleWindowSizeChange() {
+          setIsMobile(window.innerWidth <= 768);
+      }
+      useEffect(() => {
+          window.addEventListener('resize', handleWindowSizeChange);
+          return () => {
+              window.removeEventListener('resize', handleWindowSizeChange);
+          }
+      }, []);
+  
+    return (
+        <CurrentPageContext.Provider value={[currentPage, setCurrentPage]}>
+            <MobileContext.Provider value={[isMobile, setIsMobile]}>
+                <UserContext.Provider value={[user, setUser]}>
+                    <div>
+                        <div>
+                            {isMobile ? <Global_MenuBar_mobile/> : <Global_MenuBar />}
+                        </div>
+                        <div className="App">
+                            {currentPage === 'Home' && <Home/>}
+                            {currentPage === 'Upload' && !isMobile && <UploadDesktop/>}
+                            {currentPage === 'Upload' && isMobile && <UploadPhone />}
+                            {currentPage === 'Log in' && <LoginRegistration/>}
+                        </div>
+                    </div>
+                </UserContext.Provider>
+            </MobileContext.Provider>
+        </CurrentPageContext.Provider>
+      )
+  }
+  export default App;
+  
