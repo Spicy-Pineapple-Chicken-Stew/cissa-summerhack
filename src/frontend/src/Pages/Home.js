949b80dba6e050c7f920cc2f53f80912cad8d70a
Fengax <857514.leofeng@gmail.com>
2022-01-27 21:32:27 +1100

    added website description

--- src/frontend/src/Pages/Home.js
+++ src/frontend/src/Pages/Home.js
@@ -9,11 +9,162 @@ import { Box } from '@mui/system';
 import des1 from '../images/fileformaticon.png'
 import des2 from '../images/AI_summarization.png';
 import des3 from '../images/aiquestionicon2.png';
+import {MobileContext} from "../Contexts/MobileContext";
+
+
+function renderDesktopDescription(){
+    return(
+        <Box sx={{height: '100vh', marginTop: '27%', marginLeft: '-3.4vw', width: '99.01vw', bgcolor: '#6CA1F0'}}>
+            <div style={{
+                display: "flex",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "center",
+                columnGap: "10vw"
+            }}>
+                <div style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    marginTop: "10vh",
+                    alignItems: "center"
+                }}>
+                    <img src={des1} height={"220vh"} width={"250vw"}/>
+                    <Typography color={'#FFFFFF'} fontSize={'1.5vw'} fontFamily={"Oxygen"}>
+                        Support wide range of formats
+                    </Typography>
+                    <div style={{
+                        textAlign: "center",
+                        marginTop: "2vh",
+                        width: "20vw"
+                    }}>
+                        <Typography color={'#FFFFFF'} fontSize={'1vw'} fontFamily={"Oxygen"}>
+                            Bridge supports uploading a wide variety of formats from text, weblinks, youtube videos to even custom videos. Any study material can be easily uploaded and processed through our backend servers keeping your system light and free from the heavy computation.
+                        </Typography>
+                    </div>
+                </div>
+                <div style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    marginTop: "10vh",
+                    alignItems: "center"
+                }}>
+                    <img src={des2} height={"220vh"}/>
+                    <Typography color={'#FFFFFF'} fontSize={'1.5vw'} fontFamily={"Oxygen"}>
+                        AI summarization
+                    </Typography>
+                    <div style={{
+                        textAlign: "center",
+                        marginTop: "2vh",
+                        width: "20vw"
+                    }}>
+                        <Typography color={'#FFFFFF'} fontSize={'1vw'} fontFamily={"Oxygen"}>
+                            Any content can easily be summaries in comprehensive text, made available through implementation of numerous AI technologies such as GPT2 Abstractive Summary model, BERT Extractive Summary model and Bidirectional RNN punctuator model.
+
+                        </Typography>
+                    </div>
+                </div>
+                <div style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    marginTop: "10vh",
+                    alignItems: "center"
+                }}>
+                    <img src={des3} height={"220vh"}/>
+                    <Typography color={'#FFFFFF'} fontSize={'1.5vw'} fontFamily={"Oxygen"}>
+                        Automated question generation
+                    </Typography>
+                    <div style={{
+                        textAlign: "center",
+                        marginTop: "2vh",
+                        width: "20vw"
+                    }}>
+                        <Typography color={'#FFFFFF'} fontSize={'1vw'} fontFamily={"Oxygen"}>
+                            Create quick and easy flashcards with the help of automated question generation, using advanced AI models such as Structured BERT SRL predictor and NLTK Pos Tagger. Any summarized text will have questions generated based on information extracted from the text.
+
+                        </Typography>
+                    </div>
+                </div>
+            </div>
+        </Box>
+    )
+}
+
+function renderMobileDescription(){
+    return(
+        <Box sx={{marginTop: '27%', marginLeft: '-3.4vw', width: '99.01vw', bgcolor: '#6CA1F0'}}>
+            <div style={{
+                display: "flex",
+                alignItems: "center",
+                flexDirection: "column",
+                textAlign: "center"
+            }}>
+                <div style={{
+                    display: "flex",
+                    alignItems: "center",
+                    flexDirection: "column",
+                    textAlign: "center"
+                }}>
+                    <img src={des1} height={"220vh"}/>
+                    <Typography color={'#FFFFFF'} fontSize={'3vw'}>
+                        Support wide range of formats
+                    </Typography>
+                    <div style={{
+                        width: "50vw"
+                    }}>
+                        <Typography color={'#FFFFFF'} fontSize={'2vw'}>
+                            Bridge supports uploading a wide variety of formats from text, weblinks, youtube videos to even custom videos. Any study material can be easily uploaded and processed through our backend servers keeping your system light and free from the heavy computation.
+                        </Typography>
+                    </div>
+                </div>
+                <div style={{
+                    display: "flex",
+                    alignItems: "center",
+                    flexDirection: "column",
+                    textAlign: "center",
+                    marginTop: "3vh"
+                }}>
+                    <img src={des2} height={'300vh'}/>
+                    <Typography color={'#FFFFFF'} fontSize={'3vw'}>
+                        AI summarization
+                    </Typography>
+                    <div style={{
+                        width: "50vw"
+                    }}>
+                        <Typography color={'#FFFFFF'} fontSize={'2vw'}>
+                            Any content can easily be summaries in comprehensive text, made available through implementation of numerous AI technologies such as GPT2 Abstractive Summary model, BERT Extractive Summary model and Bidirectional RNN punctuator model.
+                        </Typography>
+                    </div>
+                </div>
+                <div style={{
+                    display: "flex",
+                    alignItems: "center",
+                    flexDirection: "column",
+                    textAlign: "center",
+                    marginTop: "3vh"
+                }}>
+                    <img src={des3} width={'60%'}/>
+                    <Typography color={'#FFFFFF'} fontSize={'3vw'}>
+                        Automated question generation
+                    </Typography>
+                    <div style={{
+                        width: "50vw"
+                    }}>
+                        <Typography color={'#FFFFFF'} fontSize={'2vw'}>
+                            Create quick and easy flashcards with the help of automated question generation, using advanced AI models such as Structured BERT SRL predictor and NLTK Pos Tagger. Any summarized text will have questions generated based on information extracted from the text.
+                        </Typography>
+                    </div>
+                </div>
+
+            </div>
+        </Box>
+    )
+}
 
 
 function Home(props){
     let [currentPage, setCurrentPage] = useContext(CurrentPageContext);
     let [animationState, setAnimationState] = useState(true);
+    let [isMobile, setIsMobile] = useContext(MobileContext);
 
     return(
         <div className = 'wrapper'>
@@ -43,36 +194,8 @@ function Home(props){
                     </ColorButton>
                 </Slide>
             </div>
-            <Box sx={{height: '1080px', marginTop: '27%', marginLeft: '-3.4vw', width: '99.01vw', bgcolor: '#6CA1F0'}}>
-                <Box sx={{marginTop: '9vw', height: '360px', display: 'flex', flexDirection: 'row'}}>
-                    <Box width={'30%'} sx={{position: 'relative', marginLeft: '6vw'}}>
-                        <img src={des1} width={'80%'}/>
-                    </Box>
-                    <Box width={'30%'} sx={{position: 'relative', marginLeft: '9vw'}}>
-                        <img src={des2} width={'30%'}/>
-                    </Box>
-                    <Box width={'30%'}>
-                        <img src={des3} width={'60%'}/>
-                    </Box>
-                </Box>
-                <Box sx={{marginTop: '2vw', display: 'flex', flexDirection: 'row'}}>
-                    <Box sx={{marginLeft: '6vw'}}>
-                        <Typography color={'#FFFFFF'} fontSize={'1.5vw'}>
-                            Support wide range of formats
-                        </Typography>
-                    </Box>
-                    <Box sx={{marginLeft: '15vw'}}>
-                        <Typography color={'#FFFFFF'} fontSize={'1.5vw'}>
-                            AI summarization
-                        </Typography>
-                    </Box>
-                    <Box sx={{marginLeft: '16vw'}}>
-                        <Typography color={'#FFFFFF'} fontSize={'1.5vw'}>
-                            Automated question generation
-                        </Typography>
-                    </Box>
-                </Box>
-            </Box>
+            {!isMobile && renderDesktopDescription()}
+            {isMobile && renderMobileDescription()}
         </div>
     )
 }
