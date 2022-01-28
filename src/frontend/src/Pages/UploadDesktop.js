84b34bb773d90402b8e4e2d0f25012887490aacd
thebowenfeng <857514.leofeng@gmail.com>
2022-01-28 15:08:52 +1100

    updated to HTTPS

--- src/frontend/src/Pages/UploadDesktop.js
+++ src/frontend/src/Pages/UploadDesktop.js
@@ -188,7 +188,7 @@ export default function UploadDesktop(props){
         );
 
         // Details of the uploaded file
-        axios.post("http://194.193.55.245:9000/api/file_summary", formData).then((response) => {
+        axios.post("https://194.193.55.245:9000/api/file_summary", formData).then((response) => {
             props.setTaskList([{
                 taskType: "file",
                 taskPreview: "",
